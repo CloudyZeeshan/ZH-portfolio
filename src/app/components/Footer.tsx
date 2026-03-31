4f11cd7169f0d18532c9df98f9b9f9e8885207ac
CloudyZeeshan <cloudyzeeshan@example.com>
2026-03-31 19:25:05 +0500

    fix Footer JSX parse error

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,12 +9,14 @@ const Footer = () => {
         <Link href="https://github.com/CloudyZeeshan" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
           <Image src="/github.png" alt="GitHub" className="w-full h-full" width={300} height={300}/>
         </Link>
-{/*         <Link href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10"> */}
-{/*           <Image src="/linkedin.png" alt="LinkedIn" className="w-full h-full" width={300} height={300}/> */}
+        {/*
+        <Link href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
+          <Image src="/linkedin.png" alt="LinkedIn" className="w-full h-full" width={300} height={300}/>
         </Link>
-{/*         <Link href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10"> */}
-{/*           <Image src="/facebook.png" alt="Facebook" className="w-full h-full" width={300} height={300}/> */}
+        <Link href="/" target="_blank" rel="noopener noreferrer" className="w-10 h-10">
+          <Image src="/facebook.png" alt="Facebook" className="w-full h-full" width={300} height={300}/>
         </Link>
+        */}
       </div>
       <div className="text-sm text-gray-500">&copy; {new Date().getFullYear()} Zeeshan Haider. All Rights Reserved.</div>
     </footer>
